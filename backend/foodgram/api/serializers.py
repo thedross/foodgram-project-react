142e74ed76b37e3e35a81856809f9693a317b7be
Федор Абаза <thedross@gmail.com>
2023-09-05 02:21:03 +0300

    After few days.. New edition, must work. Correct mistakes, volumes,nginx, images

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -26,7 +26,10 @@ class Base64ImageField(serializers.ImageField):
         if isinstance(data, str) and data.startswith('data:image'):
             format, imgstr = data.split(';base64,')
             ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name='photo.' + ext)
+            data = ContentFile(
+                base64.b64decode(imgstr),
+                name='foodgram.' + ext
+            )
         return super().to_internal_value(data)
 
 
